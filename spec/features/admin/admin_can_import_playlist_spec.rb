180c000eaf906cfe7890de05bffe7ddce9188e64
Jane Greene <janegreenecasa@gmail.com>
2020-07-03 17:40:21 -0600

    Youtube service still wip

--- spec/features/admin/admin_can_import_playlist_spec.rb
+++ spec/features/admin/admin_can_import_playlist_spec.rb
@@ -6,24 +6,27 @@ describe "An Admin can import playlist" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
   end
 
-  it "by importing from youtube" do
+  it "by importing from youtube more than 50 in order" do
 
     visit new_admin_tutorial_path
     # save_and_open_page
 
     fill_in 'Title', with: "New Import"
     fill_in 'Description', with: "New Import Description"
-    fill_in 'Thumbnail', with: ""
+    fill_in 'Thumbnail', with: "" #need a thumbnail
     click_on "Import YouTube Playlist"
     expect(current_path).to eq(admin_dashboard_path)
 
     expect(page).to have_content("Successfully created tutorial. View it here.")
     click_on "View it here."
-    expect(current_path).to eq(tutorial_path(tutorial))
+    expect(current_path).to eq(tutorial_path(Tutorial.last.id))
+    tutorial = Tutorial.last
+    expect(current_path).to eq(tutorial_path(tutorial.id))
 
-    within(first(".video")) do
-      expect(page).to have_content("How to tie your shoes.") #not true...need to fix
-    end
-    #could use orderly gem to test more videos and the order they appear
+    video = Video.last
+    expect(video.tutorial_id).to eq(tutorial.id)
+    expect(tutorial.videos.count).to be > 50 #need to find longer playlist
+    expect(tutorial.videos.first.title).to eq('')#
+    expect(tutorial.videos.first.last).to eq('')#
   end
 end
